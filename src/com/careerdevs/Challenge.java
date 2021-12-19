c370a9e97993a9897ef69d06fa8fcc8953fb6fde
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2021-12-19 15:18:21 -0500

    more solutions

--- src/com/careerdevs/Challenge.java
+++ src/com/careerdevs/Challenge.java
@@ -7,7 +7,7 @@ public class Challenge{
     public static int addition(int num) {
         return num + 1;
     }
-    public static int search(int arr[], int item) {
+    public static int search(int[] arr, int item) {
         int index = 0;
         for(int i = 0; i < arr.length; i++){
             if(item == arr[i]){
