611db90dd8a00b19a683faa34722e83189d093a9
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2021-12-19 15:03:04 -0500

    second solution

--- src/com/careerdevs/Challenge.java
+++ src/com/careerdevs/Challenge.java
@@ -4,4 +4,7 @@ public class Challenge{
     public static int getFirstValue(int[] arr) {
         return arr[0];
     }
+    public static int addition(int num) {
+        return num + 1;
+    }
 }
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Challenge.getFirstValue(new int[]{1, 2, 3, 4}));
+        System.out.println(Challenge.addition(4));
     }
 }
