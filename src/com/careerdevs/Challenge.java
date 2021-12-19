3bc4a02f58d2eb7ccbe55464c7c8067d83930237
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2021-12-19 15:17:44 -0500

    more solutions

--- src/com/careerdevs/Challenge.java
+++ src/com/careerdevs/Challenge.java
@@ -7,4 +7,19 @@ public class Challenge{
     public static int addition(int num) {
         return num + 1;
     }
+    public static int search(int arr[], int item) {
+        int index = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(item == arr[i]){
+                index = i;
+            }
+        }
+        if(index == 0){
+            index = -1;
+        }
+        return index;
+    }
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
 }
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //https://edabit.com/challenge/QaApgtePE6QrCZ64o
         System.out.println(Challenge.getFirstValue(new int[]{1, 2, 3, 4}));
+        //https://edabit.com/challenge/NAQhEoxbofPidLxm9
         System.out.println(Challenge.addition(4));
+        //https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+        System.out.println(Challenge.search(new int[]{1, 2, 3, 4}, 3));
+        //https://edabit.com/challenge/2t8JDxF7wLrg7yJ5E
+        System.out.println(Challenge.convert(5));
     }
 }
